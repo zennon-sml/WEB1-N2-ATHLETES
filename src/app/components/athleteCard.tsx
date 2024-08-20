31306bf08f17f94a38fa8c243184d47f41d99857
zennon-sml <zennonsampaioml@gmail.com>
2024-08-20 15:48:30 -0300

    starring function

--- src/app/components/athleteCard.tsx
+++ src/app/components/athleteCard.tsx
@@ -5,34 +5,32 @@ import { Star } from 'lucide-react'
 
 interface AthleteProps {
   athlete: Athlete
+  onFavorite: (athlete: Athlete) => void
 }
-export default function AthleteCard({ athlete }: AthleteProps) {
+
+export default function AthleteCard({ athlete, onFavorite }: AthleteProps) {
   return (
-    <div className="bg-slate-800 w-48 max-w-60  rounded-md p-2 flex flex-col items-start m-2 text-white">
+    <div className="bg-slate-800 w-48 max-w-60 rounded-md p-2 flex flex-col items-start m-2 text-white">
       <h2>{athlete.name}</h2>
       <p>Age: {athlete.age}</p>
       <p className="whitespace-nowrap w-20 flex-nowrap">
         Team: {athlete.team_name}
       </p>
-      {!athlete.imgPath ? (
-        <div className="flex items-center justify-center w-full">
-          <img
-            src="https://static.vecteezy.com/ti/vetor-gratis/p1/14151738-jogador-de-futebol-icone-simples-preto-vetor.jpg"
-            alt={athlete.name}
-            className="w-40 h-40 rounded-md"
-          />
-        </div>
-      ) : (
-        <div className="flex items-center justify-center w-full">
-          <img
-            src={athlete.imgPath}
-            alt={athlete.name}
-            className="w-40 h-40 rounded-md"
-          />
-        </div>
-      )}
       <div className="flex items-center justify-center w-full">
-        <Button className="m-2">
+        <img
+          src={
+            athlete.imgPath ||
+            'https://static.vecteezy.com/ti/vetor-gratis/p1/14151738-jogador-de-futebol-icone-simples-preto-vetor.jpg'
+          }
+          alt={athlete.name}
+          className="w-40 h-40 rounded-md"
+        />
+      </div>
+      <div className="flex items-center justify-center w-full">
+        <Button
+          className="m-2 hover:bg-amber-500"
+          onClick={() => onFavorite(athlete)}
+        >
           <Star />
           Favoritar
         </Button>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { FormEvent, useState } from 'react'
 import AthleteCard from './components/athleteCard'
 import { GetAthletes } from './utils/getAthletes'
 import { Skeleton } from '@/components/ui/skeleton'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import { Star } from 'lucide-react'
 
 export type Athlete = {
   player_id: string
@@ -14,6 +16,7 @@ export type Athlete = {
 
 const Home: React.FC = () => {
   const [athletes, setAthletes] = useState<Athlete[]>([])
+  const [starredAthletes, setStarredAthletes] = useState<Athlete[]>([])
   const [isLoading, setIsLoading] = useState(false)
   const [search, setSearch] = useState('')
 
@@ -27,9 +30,13 @@ const Home: React.FC = () => {
     setIsLoading(false)
   }
 
+  const handleFavorite = (athlete: Athlete) => {
+    setStarredAthletes((prev) => [...prev, athlete])
+  }
+
   return (
     <main className="flex min-h-screen w-full flex-col items-center bg-slate-600">
-      <nav className="w-4/5 rounded-md  gap-2 p-2 mt-5 bg-slate-800">
+      <nav className="w-4/5 rounded-md flex items-center justify-center gap-2 p-2 mt-5 bg-slate-800">
         <form
           className="flex items-center justify-center gap-2"
           onSubmit={fetchData}
@@ -37,13 +44,31 @@ const Home: React.FC = () => {
           <input
             type="text"
             placeholder="Digite o nome do jogador"
-            className="rounded-md w-6/12 p-2"
+            className="rounded-md w-80 p-2"
             onChange={(e) => setSearch(e.target.value)}
           />
-          <button type="submit" className="rounded-md w-1/12 p-2 bg-amber-500">
+          <button type="submit" className="rounded-md w-20 p-2 bg-amber-500">
             BUSCAR
           </button>
         </form>
+
+        <Dialog>
+          <DialogTrigger className="bg-amber-500 flex rounded-md p-2">
+            <Star />
+            Favoritos
+          </DialogTrigger>
+          <DialogContent className="min-w-max">
+            <div className="flex flex-nowrap w-4/5 items-center justify-center">
+              {starredAthletes.map((athlete, index) => (
+                <AthleteCard
+                  key={index}
+                  athlete={athlete}
+                  onFavorite={handleFavorite}
+                />
+              ))}
+            </div>
+          </DialogContent>
+        </Dialog>
       </nav>
       {isLoading ? (
         <div className="flex flex-wrap w-4/5 items-center justify-center gap-2 p-2">
@@ -54,7 +79,11 @@ const Home: React.FC = () => {
       ) : (
         <div className="flex flex-wrap w-4/5 items-center justify-center">
           {athletes.map((athlete, index) => (
-            <AthleteCard key={index} athlete={athlete} />
+            <AthleteCard
+              key={index}
+              athlete={athlete}
+              onFavorite={handleFavorite}
+            />
           ))}
         </div>
       )}
